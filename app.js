9f045ba4d3188a485a7808d0c1f14c98b5a3783a
mustafa <mustafatacyildiz9@gmail.com>
2022-01-21 00:02:02 +0300

    Js variables and data types

---
+++ app.js
@@ -0,0 +1,90 @@
+// console.log("I'm from file");
+
+// var x = 7;
+// var y = 5;
+
+
+// console.log({x});
+// console.log({y});
+
+// y = x
+
+// console.log({y});
+
+// alert("Hey")
+
+// var a = "Global"
+// console.log(a);
+
+// {
+//     let a = "Scope"
+//     console.log(a);
+//     {
+//         let a = "inner scope"
+//         console.log(a);
+//     }
+//     console.log(a); 
+// }
+
+// console.log(a);
+
+// let c = 3 ;
+// console.log(c);
+// c = 5;
+// console.log(c);
+
+// const h = "constant variable"
+// console.log(h);
+
+// const h ="2"  
+//error verir. ikinci defa const değer verilemez.
+
+// console.log(d);
+// var d = 3;
+// console.log(d);
+
+// console.log(e);
+// let e = 5;
+
+// var f;
+// console.log(tyoeof f);
+
+// f = 5 ;
+// console.log(typeof f);
+// console.log(typeof 5.45);
+// console.log(typeof NaN);
+// console.log(typeof "Ben bir sitringim");
+// console.log(typeof (0/1));
+// console.log(typeof typeof 5);
+// console.log(typeof (1/0));
+
+// var merhaba = 'Merhaba';
+// var selam = "Sanada selam\' Mark"
+// console.log(merhaba);
+// console.log(selam);
+
+// var instructor = "Mark"
+// var d = `Merhaba ${instructor}`
+// console.log(d);
+
+// console.log(Boolean("")); //false
+// console.log(Boolean(" ")); //true
+// console.log(Boolean(3 > 5)); //false
+// console.log(Boolean(3 < 5)); //true
+// console.log(Boolean(3 == 5)); //false
+// console.log(Boolean("0")); //dolu string olduğu için true
+
+// var s;
+// console.log(Boolean(s)); //undefined olduğu için false
+// console.log(Boolean(null)); //false
+// console.log(Boolean(typeof s)); // typeof s object tipi olduğu için true
+// console.log(null==false);
+// console.log(null==true);
+// console.log(null==null); //true
+// console.log(null==undefined); //true
+// console.log(null===null); //true
+// console.log(null===undefined); //false
+// console.log(2 == "2"); //true
+// console.log(2 ==="2"); //false
+// console.log(2 + "2"); //sonuç 22
+// console.log(2 + + "2"); // sonuç 4
