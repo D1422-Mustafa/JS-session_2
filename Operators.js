0a49d709ac310ed05b620da2fc74c5fcc16ba95c
Mustafa TAÇYILDIZ <93773148+D1422-Mustafa@users.noreply.github.com>
2022-01-22 15:04:56 +0300

    session 2 eklendi

---
+++ Operators.js
@@ -0,0 +1,113 @@
+var x = 2;
+var y = 5;
+var z = 7;
+
+var t ="2"
+
+var u = t-y
+
+// console.log(x+y);
+// console.log(z+t);
+
+// console.log(z - t);
+// console.log(t - y);
+
+// console.log(typeof u, u);
+
+// console.log(z/t);
+
+// console.log(5%2);  //sonuç 1 olur.
+
+// var a = 5;
+// var b = 0;
+
+// console.log(a/b)
+
+// console.log(typeof Infinity);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+
+// var c = 100+50*3
+// console.log(c);
+
+// //ıncreament
+
+// var a = 10;
+// console.log(a++);
+// console.log(a);
+
+// var b = 5;
+// console.log(++b);
+
+//Decreament
+
+// var c= 25;
+// console.log(c--);
+// console.log(c);
+
+// var d = 38;
+// console.log(--d);
+
+// e = d++
+// console.log({e},{d});
+
+// e = ++d
+// console.log({e},{d});
+
+// console.log(d++ + ++d);
+
+// var input = +prompt("Enter age");
+// console.log(input, typeof input);
+
+// var c = "8";
+
+// console.log(-c);
+
+// var a = 4;
+// var b = "4";
+// var c = a == b
+// console.log(a == b);
+// console.log(c);
+
+// console.log(a!==b);
+
+// var a = "a";
+// var b = 20;
+// console.log(a.charCodeAt());
+// console.log("2".charCodeAt());
+
+// console.log(b > a);
+
+// console.log("fullStack" > "fullstack");
+
+// var a = (2 || 3);
+// var a = (0 || 3);
+
+// console.log(a);
+
+// var b = (0 && false)
+
+// var b = (2 && 3);
+// console.log(b);
+
+// console.log(null && NaN);
+
+// var capitalLetter = true;
+// var symbol = true;
+// var passLemght = false;
+
+// var result = capitalLetter && symbol && passLemght;
+// console.log(result || console.log("Try again"));
+
+// var a;
+
+// console.log(a ?? "Null veya undefined");
+
+// var b = null;
+// console.log(b ?? "Null veya undefined");
+
+// console.log(a ?? b);
+
+var c = 1;
+
+console.log(c ?? "Null veya undefined");
